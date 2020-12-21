aacc68ac0928c1378b885f508e1cd6e3454c4b80
Carlos <cdsjg@iscte.pt>
2020-12-21 01:26:17 +0000

    done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import itertools
+import math
 import random as rand
 import numpy as num
 
@@ -7,13 +8,14 @@ import numpy as num
 
 k = 11
 
+combinations_10_all = list(map(list, itertools.product([0, 1], repeat=10)))
+
 
 def return_1st(val):
     return val[0]
 
 
 def make_sets():
-    combinations_10_all = list(map(list, itertools.product([0, 1], repeat=10)))
     training_set = []
     test_set = []
     num.random.shuffle(combinations_10_all)
@@ -59,4 +61,45 @@ def runner():
     print_best(values)
 
 
-runner()
+def splitting_lists():
+    set1 = []
+    set2 = []
+    all_set = combinations_10_all
+    for x in combinations_10_all:
+        if x[1] == 0:
+            set1.append(x)
+        else:
+            set2.append(x)
+
+    entropy_1 = entropy(set1)
+    entropy_2 = entropy(set2)
+    entropy_all = entropy(all_set)
+
+
+def gain(d, a):
+    total = 0
+    for x in a:
+        total += sum(x) / sum(d) * entropy(x)
+
+    ganho = entropy(d) - total
+    return ganho
+
+
+def entropy(set1):
+    num_negative = 0
+    num_positve = 0
+
+    for x in set1:
+        for tt in x:
+            if tt < 0:
+                num_negative += 1
+
+            else:
+                num_positve += 1
+
+    percentagem_negative = num_negative / 100
+    percentagem_positive = num_positve / 100
+    value = - percentagem_positive * math.log(percentagem_positive, 2) - percentagem_negative * math.log(
+        percentagem_negative, 2)
+
+    return value
